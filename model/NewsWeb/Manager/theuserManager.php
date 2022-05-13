940c44631fd6ff4d9f6404059077ec88384469a0
24manu <manuel.mouzelard@hotmail.com>
2022-05-13 16:21:38 +0200

    changement

--- model/NewsWeb/Manager/theuserManager.php
+++ model/NewsWeb/Manager/theuserManager.php
@@ -13,7 +13,7 @@ class theuserManager implements ManagerInterface
     // de la table permission en jointure interne : tous
     public function theuserSelectOneById(int $id): array|bool
     {
-
+      
     }
 
     // se connecter et vérifier la validité du login/pwd, renvoie un tableau contenant les information de theuser et de permission, (sans mots de passes ni infos dangereuses), ou false
